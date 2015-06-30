b3330662faada824b5cd30c3e46cbf88d3850774
freedomgll <freedomgll@163.com>
2015-06-30 11:05:57 +0800

    Improved security

--- src/Acme/DemoBundle/Controller/LoginController.php
+++ src/Acme/DemoBundle/Controller/LoginController.php
@@ -54,4 +54,24 @@ class LoginController extends Controller
         print("logoutAction");
     }
 
+    /**
+     * @Route("/hello", defaults={"name"="World"}),
+     * @Route("/hello/{name}", name="_demo_secured_hello")
+     * @Template("AcmeDemoBundle:Secured:hello.html.twig")
+     */
+    public function helloAction($name)
+    {
+        return array('name' => $name);
+    }
+
+    /**
+     * @Route("/hello/admin/{name}", name="_demo_secured_hello_admin")
+     * @Security("is_granted('ROLE_ADMIN')")
+     * @Template("AcmeDemoBundle:Secured:helloadmin.html.twig")
+     */
+    public function helloadminAction($name)
+    {
+        return array('name' => $name);
+    }
+
 }
